c246abb3e714b243aa9c798fb991a1785a63606d
krv006 <rvkamronbek@gmail.com>
2025-11-21 23:20:36 +0500

    change

--- apps/serializers.py
+++ apps/serializers.py
@@ -6,4 +6,4 @@ from apps.models import Order
 class OrderSerializer(ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'costumer_name', 'address', 'total_cost', 'payment_method']
+        fields = ('id', 'costumer_name', 'address', 'total_cost', 'payment_method')
